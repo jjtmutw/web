26543146cb98d663c2f4dbb121a0b516004cec42
jjtmutw <jjtmutw@gmail.com>
2026-03-16 21:11:30 +0800

    Add files via upload

---
+++ board/sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "hiot-board-pwa-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./board_voice_pwa.html",
+  "./control_pwa.html",
+  "./manifest.webmanifest",
+  "./tmutitle.png",
+  "./comlogo.png",
+  "./jj.jpg",
+  "https://unpkg.com/mqtt/dist/mqtt.min.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          if (!response || response.status !== 200) {
+            return response;
+          }
+
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match("./board_voice_pwa.html"));
+    })
+  );
+});
